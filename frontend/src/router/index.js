042d74a6e194e204bd70017526a7c7b80da1fa47
jinny <kwondajin0214@gmail.com>
2025-05-14 10:08:11 +0900

    Change root path to SearchView

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-  { path: '/', name: 'Home', component: () => import('../views/LoginView.vue') },
+  { path: '/', name: 'Home', component: () => import('../views/RecipeSearchView.vue') },
   { path: '/login', name: 'Login', component: () => import('../views/LoginView.vue') },
   { path: '/search', name: 'Search', component: () => import('../views/RecipeSearchView.vue') },
   { path: '/oauth2/redirect', name: 'OAuth2Redirect', component: () => import('../components/HelloWorld.vue')}
